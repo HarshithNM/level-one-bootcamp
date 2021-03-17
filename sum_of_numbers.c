d156d8fb688d732cdfd087f0f0c90d23381dd485
HarshithNM <76142592+HarshithNM@users.noreply.github.com>
2021-03-17 14:17:11 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,16 +1,54 @@
 //Write a program to find the sum of n different numbers using 4 functions
-#include <stdio.h>
+#include<stdio.h>
+
+int limit();
+int* read();
+int calc();
+void display();
+
+
 int main()
 {
-   int n, sum = 0, c,add_num;
-   printf("How many number(s)? ");
-   scanf("%d", &n);
-   printf("Enter %d numbers:",n);
-    for (c = 1; c <= n; c++)
-  {
-    scanf("%d", &add_num);
-    sum = sum + add_num;
-  }
-    printf("Sum = %d\n", sum);
-    return 0;
+    int n,sum=0;
+    n=limit();
+    int arr[n];
+    int* ptr=read(n,arr);
+    sum=calc(n,ptr);
+    display(sum);  
+}
+
+
+int limit()
+{
+    int n;
+    printf("Enter the number  : ");
+    scanf("%d",&n);
+    return n;
+}
+
+int* read(int n,int arr[])
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the %d element : ",i);
+        scanf("%d",&arr[i]);
+       
+    }
+    return arr;
+}
+
+int calc(int n,int *ptr)
+{
+    int i,sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum = sum + *(ptr+i);
+    }
+    return sum;
+}
+
+void display(int sum)
+{
+    printf("sum of the given numbers is : %d",sum);
 }

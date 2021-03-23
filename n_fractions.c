fa89721667c086852b9502a5cc8bdbdcfd028389
HarshithNM <76142592+HarshithNM@users.noreply.github.com>
2021-03-23 13:58:17 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,52 @@
 //WAP to find the sum of n fractions.
+#include<stdio.h>
+struct fract
+{
+int num;
+int den;
+};
+typedef struct fract fr;
+fr input()
+{
+fr a;
+printf("Enter the numerator\n");
+scanf("%d",&a.num);
+printf("Enter the denominator\n");
+scanf("%d",&a.den);
+return a;
+}
+fr sum(fr a,fr b)
+{
+fr res;
+if(a.den==b.den)
+{
+res.den=a.den;
+res.num=a.num+b.num;
+}
+else
+{
+res.den=a.den*b.den;
+res.num=(a.num*b.den)+(b.num*a.den);
+}
+return res;
+}
+int main()
+{
+int n;
+printf("Enter the number of fractions\n");
+scanf("%d",&n);
+fr c,a[n];
+c.num=0;
+c.den=1;
+for(int i=0;i<n;i++)
+{
+printf("For fraction %d ",(i+1));
+a[i]=input();
+}
+for(int i=0;i<n;i++)
+{
+c=sum(c,a[i]);
+}
+printf("The final sum is %d/%d which is %.2f ",c.num,c.den,(c.num/(1.0*c.den))*100);
+return 0;
+}
